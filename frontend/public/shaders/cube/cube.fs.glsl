fe6f12fe74848b460316cb73471153fb00ef1ae3
kris temmerman <kris@neuroproductions.be>
2023-05-24 07:55:39 +0200

    fix

--- frontend/public/shaders/cube/cube.fs.glsl
+++ frontend/public/shaders/cube/cube.fs.glsl
@@ -5,7 +5,7 @@ varying vec3 vNormal;
 uniform vec3 lightDir;
 uniform vec4 color;
 void main() {
-    float l =dot(vNormal,lightDir)*0.5+0.5;
+    float l =dot(normalize(vNormal),lightDir)*0.5+0.5;
     l*=0.9;
     l+=0.1;
     gl_FragColor = vec4(l*color.xyz,1.0);
